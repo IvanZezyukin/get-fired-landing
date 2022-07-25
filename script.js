57a2b48f047ea891b60746a913c72cb4d2f69602
IvanZezyukin <i.zezukin@gmail.com>
2022-07-25 17:27:44 +0300

    email picker added

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const showMessage = (arrayOfMessages, indexOfMessage) => {
     if (indexOfMessage >= 13) {
         downButton.innerText = 'ЗАРЕГИСТРИРОВАТЬСЯ';
         downButton.removeEventListener("click", showNextMessage);
-        downButton.addEventListener('click', registerUser);
+        downButton.addEventListener('click', showPopUp);
     }
 };
 
@@ -55,3 +55,75 @@ function updateScroll() {
     };
 }
 window.addEventListener("scroll", updateScroll);
+
+//popup
+const hidePopUp = () => {
+    document.getElementById('popup').style.display ='none';
+}
+const showPopUp = () => {
+    document.getElementById('popup').style.display ='block';
+};
+
+
+// form handler
+
+async function handleFormSubmit(event) {
+    event.preventDefault()
+    const data = serializeForm(event.target)
+    toggleLoader()
+    const { status, error } = await sendData(data)
+    toggleLoader()
+    if (status === 0) {
+        onSuccess(event.target)
+    } else {
+        onError(error)
+    }
+}
+
+function serializeForm(formNode) {
+    const { elements } = formNode
+    const data = Array.from(elements)
+      .filter((item) => !!item.name)
+      .map((element) => {
+          const { name, value } = element
+          return { name, value }
+      })
+    return data;
+}
+
+const applicantForm = document.getElementById('form');
+applicantForm.addEventListener('submit', handleFormSubmit);
+
+async function sendData(data) {
+    const email = data[0].value;
+    return await fetch(`https://functions.yandexcloud.net/d4ekhme29ffedq361qt0?email=${email}`, {
+        mode: 'no-cors',
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+        // body есть при post запросах
+        //body: JSON.stringify(data),
+    })
+}
+
+function toggleLoader() {
+    const loader = document.getElementById('loader')
+    loader.classList.toggle('hidden')
+}
+
+function onSuccess(formNode) {
+    // alert('Ваша заявка отправлена!');
+    formNode.classList.toggle('hidden');
+    document.getElementById('form_success').style.display = 'block';
+}
+
+function onError(error) {
+    alert(error)
+}
+
+function checkValidity(event) {
+    const formNode = event.target.form
+    const isValid = formNode.checkValidity()
+    formNode.querySelector('button').disabled = !isValid
+}
+
+applicantForm.addEventListener('input', checkValidity)
